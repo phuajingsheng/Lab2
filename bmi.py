b54a6afe8d72e71be5abf8c094e840c7386fc6f6
Joel Phua <jingsheng10.22@ichat.sp.edu.sg>
2023-05-04 11:34:15 +0800

    hi

--- bmi.py
+++ bmi.py
@@ -9,9 +9,13 @@ def calculate_bmi (height, weight) :
     bmi = weight / (height * height)
     if bmi < 18.5:
         print("You are underweight")
+        return -1
     elif bmi >= 18.5 and bmi <= 25.0:
         print("You have a normal weight")
+        return 0
     else:
         print("You are overweight")
-
+        return 1
 calculate_bmi(weight=57, height=1.73)
+
+
